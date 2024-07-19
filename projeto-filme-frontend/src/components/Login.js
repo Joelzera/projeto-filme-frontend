a91ecdd35897e6de9b1e2b12fee987d38c53223e
joel bispo <joelbispo04@hotmail.com>
2024-07-19 10:53:24 -0300

    caracterização inicial do site

--- projeto-filme-frontend/src/components/Login.js
+++ projeto-filme-frontend/src/components/Login.js
@@ -1,7 +1,16 @@
+import Grid from '@mui/material/Grid';
+import LabTabs from './TabPanel';
 
-
-const Login = () =>{
-    return <h1>Movie Verso</h1>
+const Login = () => {
+    return (
+        <>
+            <Grid container sx={{ backgroundColor: '#2a2a2a', maxWidth: '100%', maxHeight: '100%', display: 'flex', justifyContent: 'center'}}>
+                <Grid item>
+                    <LabTabs/>
+                </Grid>
+            </Grid>
+        </>
+    )
 }
 
 export default Login;
